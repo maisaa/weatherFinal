2c9b7010405fa0b4ab8db8f0e8478c7c8c65610d
maisaa <maisaa.alkhedr@gmail.com>
2017-11-14 19:48:15 +0200

    add the api url and the form in php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <!doctype html>
+
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
@@ -63,11 +64,11 @@
                 margin-bottom: 30px;
             }
         </style>
-      
+        <link rel="stylesheet" href="<?php echo asset('css/main.css');?>"  type="text/css">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            <!-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -76,12 +77,51 @@
                         <a href="{{ route('register') }}">Register</a>
                     @endauth
                 </div>
-            @endif
+            @endif -->
 
             <div class="content">
-                <div class="title m-b-md ">
+                <div class="title m-b-md myColor">
                    Weather Forecast
                 </div>
+
+                <form class="title m-b-md myColor" name="weatherForm"  method="GET">
+                   <h1> Your weather today </h1>
+                   City: <input type="text" name="city" placeholder="city"/><br />
+                   
+                   <input type="submit" name="submit" value="Submit" />
+                </form>
+                <br />
+                <br />
+<?php
+  if(isset($_GET['city']) ? $_GET['city'] : ''){
+      $user_city = $_GET['city'];
+
+
+      $api_url =   "http://api.openweathermap.org/data/2.5/weather?q=".$user_city."&appid=4f127a6589b5a52dadae0e50a75d03b6";
+      $weather_data = file_get_contents($api_url);
+      $json = jeson_decode($weather_data, TRUE);
+
+
+      $user_temp =$json['main']['temp'];
+
+
+      echo "<strong>City:</strong>".$user_city."<br />";
+      echo "<strong>City tempreture:</strong>".$user_temp."<br />";
+  };
+
+?>
+
+<?php 
+   if(isset($_GET['submit'])){
+
+    echo $user_temp;
+
+     $data = "data.json";
+     echo $data;
+    $json_string =json_decode($_GET, JSON_PRETTY_PRINT);
+    file_put_contents($data, $json_string, FIL_APPEND);
+}
+?>
 
                 <!-- <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
